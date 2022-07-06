641c6062022cfa100a99a33304a1b06c144bf0d1
arpitbharadwaj1 <arpit.bharadwaj@sendinblue.com>
2022-07-06 15:43:16 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+    <Navbar title="Textutils" aboutText="About Textutils"/>
+    {/* <Navbar></Navbar> */}
+    </>
+  );
+}
+
+export default App;
